07b677cdea4f102e60e106c39cd7a4f9ad6c5447
jhon pardo <jhonedupardo15@gmail.com>
2025-12-18 12:13:44 -0500

    problema05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,33 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+    public static void main(String[] args) {
+     
+        String[][] estudiantes = {
+            {"Sarah Ward", "Philip Payne"},
+            {"Carrie Burton", "Lauren Rice"},
+            {"Emma Escobar", "Lori Flores"},
+            {"Steven West", "Toni Martin"}
+        };
+
+        String reporte = "";
+
+        for (int fila = 0; fila < estudiantes.length; fila++) {
+            for (int col = 0; col < estudiantes[fila].length; col++) {
+
+                String nombre = estudiantes[fila][col];
+                String inicial_l = nombre.substring(0, 1);
+
+              if (inicial_l.equals("S") || inicial_l.equals("P") || 
+                      inicial_l.equals("T")) {
+                    reporte = String.format("%s%s\n", reporte, nombre);
+                }
+            }
+        }
+
+        System.out.println("Reporte de estudiantes que Inician con letras:"
+                + " S, P o T:");
+        System.out.println(reporte);
+    }
+
 }
